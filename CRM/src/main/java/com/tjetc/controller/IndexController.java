4b6da8f4540ee8bb03a1fabc46161be727ea7090
chibiao <1992999@qq.com>
2019-07-16 09:02:21 +0800

    项目模版

---
+++ CRM/src/main/java/com/tjetc/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.tjetc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/index")
+	public String index() {
+		System.out.println("1111111");
+		return "main";
+	}
+
+	@RequestMapping("/second")
+	public String second() {
+		return "second";
+	}
+}
